30ed44d26e2b639856cd733a7cf036e20b0a06c6
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-05-24 16:15:49 -0700

    created header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Header from '../components/Header/index';
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
         <title>UP Boutique</title>
       </Head>
       <div className='page-wrapper'>
+        <Header />
         <main>
           <h2>Hello from UP Boutique!</h2>
         </main>
